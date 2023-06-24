94043df363b41dd278da86759c454bef4a3b4fc4
Ritvik Mahajan <ritvikmahajan2001@gmail.com>
2023-06-25 00:47:35 +0530

    feat: add dashboard ui

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const LoginPage = () => {
+  return (
+    <div className="flex w-[100vw] h-[100vh]">
+      <div className="flex items-center justify-center w-[40vw] h-full bg-black">
+        <div className="font-bold text-white text-7xl font-montserrat">
+          Board.
+        </div>
+      </div>
+      <div className="w-[60vw] flex justify-center items-center">
+        <div>Sign In</div>
+      </div>
+    </div>
+  );
+};
